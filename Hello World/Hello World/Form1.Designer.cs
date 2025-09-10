bdd646219683553249a9646262c9169402b930f9
De Klein, Marcel <marcel.deklein@cadmes.com>
2025-09-10 11:48:18 +0200

    Box added

--- Hello World/Hello World/Form1.Designer.cs
+++ Hello World/Hello World/Form1.Designer.cs
@@ -28,33 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(701, 355);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(138, 26);
+            this.button1.Location = new System.Drawing.Point(579, 315);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(190, 55);
-            this.button1.TabIndex = 0;
+            this.button1.Size = new System.Drawing.Size(116, 34);
+            this.button1.TabIndex = 1;
             this.button1.Text = "&Ok";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(725, 379);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Main Menu";
+            this.groupBox1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
+        private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.Button button1;
     }
 }
