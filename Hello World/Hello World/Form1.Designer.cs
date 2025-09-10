1a3c1567ab2f07b831182414c3911bb336f5032f
De Klein, Marcel <marcel.deklein@cadmes.com>
2025-09-10 11:46:07 +0200

    Buttons ordered

--- Hello World/Hello World/Form1.Designer.cs
+++ Hello World/Hello World/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(299, 38);
+            this.button1.Location = new System.Drawing.Point(65, 32);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(190, 55);
             this.button1.TabIndex = 0;
@@ -44,11 +44,11 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(320, 136);
+            this.button2.Location = new System.Drawing.Point(65, 102);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(176, 43);
+            this.button2.Size = new System.Drawing.Size(190, 53);
             this.button2.TabIndex = 1;
-            this.button2.Text = "button2";
+            this.button2.Text = "&Cancel";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
@@ -59,7 +59,7 @@
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Main Menu";
             this.ResumeLayout(false);
 
         }
